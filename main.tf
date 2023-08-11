6fc5439e9fc30076ad0dbe671327921769c22871
ibrahim <iirasras@outlook.com>
2023-08-11 12:22:30 +1000

    Create storage account

--- main.tf
+++ main.tf
@@ -14,3 +14,19 @@ provider "azurerm" {
   subscription_id = "5ffdafb5-1a88-4605-9a42-37263853cb53"
   tenant_id       = "3154c2b2-568c-4d0f-8354-8dad5211339c"
 }
+
+resource "azurerm_resource_group" "sablobrg" {
+  name     = "smart-active"
+  location = " Australia East"
+}
+resource "azurerm_storage_account" "terraform" {
+  name                     = "saterraformsa"
+  location                 = azurerm_resource_group.sablobrg.location
+  resource_group_name      = azurerm_resource_group.sablobrg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+
+
+
+}
